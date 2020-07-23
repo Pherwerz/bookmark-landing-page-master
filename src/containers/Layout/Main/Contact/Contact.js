9e5c04838373c02d785cc8a5004da67755b35cf1
pherwerz <lordrisk101@gmail.com>
2020-07-23 10:21:33 +0100

    initial commit

--- src/containers/Layout/Main/Contact/Contact.js
+++ src/containers/Layout/Main/Contact/Contact.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import classes from './Contact.module.scss';
+import Button from '../../../../components/Button/Button';
 
 const Contact = () => {
   return (
     <section className={classes.Contact}>
-      {
-        // 35,000+ already joined
-        // Stay up-to-date with what we’re doing
-        // Contact Us
-      }
+      <div className={classes.ContactBox}>
+        <p>35,000+ already joined</p>
+
+        <h2>Stay up-to-date with what we’re doing</h2>
+
+        <form className={classes.ContactForm}>
+          <input
+            type="text"
+            placeholder="Enter your Email address"
+            className={classes.ContactInput}
+          />
+          <Button type="orange" name="Contact Us" />
+        </form>
+      </div>
     </section>
   );
 };
