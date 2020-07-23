3eb6ca0816b84499e40015a8efba7a8cb9729d6c
pherwerz <lordrisk101@gmail.com>
2020-07-23 12:03:25 +0100

    initail commit

--- src/containers/Layout/Main/FAQs/FAQs.js
+++ src/containers/Layout/Main/FAQs/FAQs.js
@@ -5,57 +5,64 @@ import Container from '../../../Container/Container';
 import classes from './FAQs.module.scss';
 import arrow from '../../../../images/arrows.svg';
 
-const Quest = [
-  {
-    Q: 'What is Bookmark?',
-    A:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.'
-  },
-  {
-    Q: 'How can I request a new browser?',
-    A:
-      'Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.'
-  },
-  {
-    Q: 'Is there a mobile app?',
-    A:
-      'Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed  sollicitudin ex et ultricies bibendum.'
-  },
-  {
-    Q: 'What about other Chromium browsers?',
-    A:
-      'Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque non ut velit.'
-  }
-];
-
 class FAQs extends Component {
   state = {
-    ans: [
+    Quest: [
       {
-        test: true,
+        Q: 'What is Bookmark?',
+        icon: arrow + '#icon-chevron-down',
+        ifill: 'var(--soft-blue)',
+        classs: classes.FAQsAnshide,
         A:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.'
       },
       {
-        test: true,
+        Q: 'How can I request a new browser?',
+        icon: arrow + '#icon-chevron-down',
+        ifill: 'var(--soft-blue)',
+        classs: classes.FAQsAnshide,
         A:
           'Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.'
       },
       {
-        test: true,
+        Q: 'Is there a mobile app?',
+        icon: arrow + '#icon-chevron-down',
+        ifill: 'var(--soft-blue)',
+        classs: classes.FAQsAnshide,
         A:
           'Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed  sollicitudin ex et ultricies bibendum.'
       },
       {
-        test: true,
+        Q: 'What about other Chromium browsers?',
+        icon: arrow + '#icon-chevron-down',
+        ifill: 'var(--soft-blue)',
+        classs: classes.FAQsAnshide,
         A:
           'Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque non ut velit.'
       }
     ]
   };
 
-  clicked = e => {
-    // const id = parseInt(e.target.id);
+  clicked = id => {
+    this.setState({
+      Quest: this.state.Quest.map((cur, i) => {
+        if (id === i) {
+          cur.classs === classes.FAQsAnshide
+            ? (cur.classs = null)
+            : (cur.classs = classes.FAQsAnshide);
+
+          cur.icon === arrow + '#icon-chevron-down'
+            ? (cur.icon = arrow + '#icon-chevron-up')
+            : (cur.icon = arrow + '#icon-chevron-down');
+
+          cur.ifill === 'var(--soft-blue)'
+            ? (cur.ifill = 'var(--soft-red)')
+            : (cur.ifill = 'var(--soft-blue)');
+        }
+
+        return cur;
+      })
+    });
   };
 
   render() {
@@ -71,28 +78,30 @@ class FAQs extends Component {
 
         <Container>
           <div className={classes.FAQsList}>
-            {Quest.map((cur, i) => (
+            {this.state.Quest.map((cur, i) => (
               <div className={classes.FAQsItem} key={i}>
                 <button
                   className={classes.FAQsQuest}
-                  onClick={this.clicked}
-                  id={i}
+                  onClick={() => this.clicked(i)}
                 >
                   <h3>{cur.Q}</h3>
-                  <svg>
-                    <use
-                      xlinkHref={
-                        arrow + `#icon-chevron-down`
-                      }
-                    />
+                  <svg style={{ fill: cur.ifill }}>
+                    <use xlinkHref={cur.icon} />
                   </svg>
                 </button>
-                <div className={classes.FAQsAns}>
-                  <p>{this.state.ans[i].A}</p>
+
+                <div
+                  className={[
+                    classes.FAQsAns,
+                    cur.classs
+                  ].join(' ')}
+                >
+                  <p>{cur.A}</p>
                 </div>
               </div>
             ))}
           </div>
+
           <Button type="blue" name="More Info" />
         </Container>
       </section>
