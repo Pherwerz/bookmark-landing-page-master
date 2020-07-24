13d6b8bc5b1326da31801a30747ee2446367f7e7
pherwerz <lordrisk101@gmail.com>
2020-07-24 05:34:24 +0100

    another commit

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import Icon from '../../images/logo-bookmark.svg';
+import Icon2 from '../../images/logo-bookmark-white.svg';
 import classes from './Logo.module.scss';
 
 const Logo = props => {
   let type;
 
   if (props.type === 'dark') {
-    type = 'svg';
+    type = Icon;
   } else if (props.type === 'light') {
-    type = 'svg2';
+    type = Icon2;
   }
 
   return (
     <div className={classes.Logo}>
-      <svg className={classes.Logoicon}>
-        <use xlinkHref={Icon + `#${type}`}></use>
-      </svg>
+      <img
+        className={classes.Logoicon}
+        src={type}
+        alt="Logo"
+      />
     </div>
   );
 };
--- src/containers/Layout/Main/Contact/Contact.js
+++ src/containers/Layout/Main/Contact/Contact.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Contact.module.scss';
 import Button from '../../../../components/Button/Button';
 
-const Contact = () => {
-  return (
-    <section className={classes.Contact}>
-      <div className={classes.ContactBox}>
-        <p>35,000+ already joined</p>
+class Contact extends Component {
+  state = { value: '' };
 
-        <h2>Stay up-to-date with what we’re doing</h2>
+  submitted = () => {};
 
-        <form className={classes.ContactForm}>
-          <input
-            type="text"
-            placeholder="Enter your Email address"
-            className={classes.ContactInput}
-          />
-          <Button type="orange" name="Contact Us" />
-        </form>
-      </div>
-    </section>
-  );
-};
+  inputhandler = e => {
+    this.setState({ value: e.target.value });
+  };
+
+  render() {
+    return (
+      <section className={classes.Contact}>
+        <div className={classes.ContactBox}>
+          <p>35,000+ already joined</p>
+
+          <h2>Stay up-to-date with what we’re doing</h2>
+
+          <form
+            className={classes.ContactForm}
+            onSubmit={this.submitted}
+          >
+            <input
+              type="email"
+              placeholder="Enter your Email address"
+              className={classes.ContactInput}
+              onChange={this.inputhandler}
+              value={this.state.value}
+            />
+            <Button type="orange" name="Contact Us" />
+          </form>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Contact;
