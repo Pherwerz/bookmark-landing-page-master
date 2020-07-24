df2bdd4eada909abb6db08075036d9487dbe710b
pherwerz <lordrisk101@gmail.com>
2020-07-24 06:07:30 +0100

    updated

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import classes from './Tab.module.scss';
 import Button from '../Button/Button';
-import Container from '../../containers/Container/Container';
 
 const Tab = props => (
   <div className={classes.Tab}>
-    <div className={classes.Tabbg} />
-    <Container>
-      <div className={classes.TabItem}>
-        <div className={classes.Tableft}>
-          <img src={props.srcs} alt="illustartion" />
-        </div>
-
-        <div className={classes.Tabright}>
-          <div className={classes.TabrightCon}>
-            <h2>{props.h}</h2>
-            <p>{props.p}</p>
-            <Button name="More info" type="blue" />
-          </div>
-        </div>
+    <div className={classes.Tableft}>
+      <div className={classes.Tabbg} />
+      <div className={classes.TableftCon}>
+        <img src={props.srcs} alt="illustartion" />
       </div>
-    </Container>
+    </div>
+
+    <div className={classes.Tabright}>
+      <div className={classes.TabrightCon}>
+        <h2>{props.h}</h2>
+        <p>{props.p}</p>
+        <Button name="More info" type="blue" />
+      </div>
+    </div>
   </div>
 );
 
